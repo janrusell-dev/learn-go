7c353353d2fbca9cabf13a8b55499077390b51bb
janrusell <janruselldev@gmail.com>
2025-12-04 02:17:05 +0800

    day 2

--- learn/range-over-iterators.go
+++ learn/range-over-iterators.go
@@ -1,1 +1,51 @@
 package learn
+
+import (
+	"fmt"
+	"iter"
+	"slices"
+)
+
+func (lst *List[T]) All() iter.Seq[T] {
+	return func(yield func(T) bool) {
+		for e := lst.head; e != nil; e = e.next {
+			if !yield(e.val) {
+				return
+			}
+		}
+	}
+}
+
+func genFib() iter.Seq[int] {
+	return func(yield func(int) bool) {
+		a, b := 1, 1
+
+		for {
+			if !yield(a) {
+				return
+			}
+			a, b = b, a+b
+		}
+	}
+}
+
+func RangeOverIterators() {
+	lst := List[int]{}
+	lst.Push(10)
+	lst.Push(69)
+	lst.Push(100)
+
+	for e := range lst.All() {
+		fmt.Println(e)
+	}
+
+	all := slices.Collect(lst.All())
+	fmt.Println("all:", all)
+
+	for n := range genFib() {
+		if n >= 10 {
+			break
+		}
+		fmt.Println(n)
+	}
+}
