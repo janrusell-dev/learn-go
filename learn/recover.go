5530fc61f9b88b552fa880bdb8c9bdae50739f0a
janrusell <janruselldev@gmail.com>
2025-12-06 02:59:49 +0800

    day 4

--- learn/recover.go
+++ learn/recover.go
@@ -1,5 +1,18 @@
 package learn
 
+import "fmt"
+
+func mayPanic() {
+	panic("asdasd")
+}
 func Recover() {
 
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered. Error:\n", r)
+		}
+	}()
+	mayPanic()
+
+	fmt.Println("After mayPanic()")
 }
